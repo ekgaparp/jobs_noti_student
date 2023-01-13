dc14e11c1b8c2cc8325e96c5b12ad7eb1ea4f496
pleng <planggao05@gmail.com>
2023-01-13 16:07:56 +0700

    fix: 🐛 edit and add validate and save

---
+++ lib/model/data_model.dart
@@ -0,0 +1,10 @@
+class DataModel {
+  String subjectId;
+  String subjectName;
+  String teacherName;
+  String date;
+  String time;
+  String detailData;
+  DataModel(this.date, this.detailData, this.subjectId, this.subjectName,
+      this.teacherName, this.time);
+}
